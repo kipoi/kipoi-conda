957ceffed2bd27b8a3d5848a3860ad2e6c557239
DerThorsten <derthorstenbeier@gmail.com>
2019-08-06 15:55:00 +0200

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 desc = "kipoi-conda: conda/pip related functionality used by Kipoi"
 setup(
     name='kipoi_conda',
-    version='0.2.1',
+    version='0.2.2',
     description=desc,
     author="Kipoi team",
     author_email='avsec@in.tum.de',
