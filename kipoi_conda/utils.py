1a14a27d003ba15e25f189b80fbeeb2d80bdf4f2
Haimasree <haimasree.de@gmail.com>
2022-03-14 20:00:14 +0100

    removing future

--- kipoi_conda/utils.py
+++ kipoi_conda/utils.py
@@ -2,9 +2,6 @@
 
 Reusing code from: https://github.com/conda/conda-api/blob/master/conda_api.py
 """
-from __future__ import absolute_import
-from __future__ import print_function
-
 import os
 import json
 import sys
