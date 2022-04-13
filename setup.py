99c1fc0ecb4535f767f7a1fcd12d4fdff35b0627
Haimasree <haimasree.de@gmail.com>
2022-04-13 15:40:15 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 desc = "kipoi-conda: conda/pip related functionality used by Kipoi"
 setup(
     name='kipoi_conda',
-    version='0.3.0',
+    version='0.3.1',
     description=desc,
     author="Kipoi team",
     author_email='avsec@in.tum.de',
